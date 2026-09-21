124c81a791c55450d1dfd8cff4d84d7aa2f0fa84
Shubham Kr. Yadav <shubhamyadav5594@gmail.com>
2026-09-21 12:53:14 +0530

    Use Vercel rewrite proxy to avoid mixed-content HTTPS/HTTP issue

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: process.env.REACT_APP_API_BASE_URL || "http://localhost:9191/api",
+  baseURL: "/api",
 });
 
 apiClient.interceptors.request.use((config) => {
