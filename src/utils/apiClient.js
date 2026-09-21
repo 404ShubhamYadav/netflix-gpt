a98b6dbad031f6246870dc2c81d0e44741cfb2f4
Shubham Kr. Yadav <shubhamyadav5594@gmail.com>
2026-09-21 14:45:43 +0530

    added duck dns

--- src/utils/apiClient.js
+++ src/utils/apiClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "/api",
+  baseURL: "https://netflixgptbackend.duckdns.org/api",
 });
 
 apiClient.interceptors.request.use((config) => {
